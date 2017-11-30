a2d7f289fbf5f3a6947d53e40243eaef89e38444
Jeky Cui <jeky.cui@ek3.com>
2017-11-30 15:38:12 -0500

    ahhhh one month

--- README.md
+++ README.md
@@ -50,6 +50,8 @@ git pull
 
 ## List
 
+* 2017-11-30: [chain.py](chain.py)
+
 * 2017-11-29: [complex.py](complex.py)
 
 * 2017-11-28: [str_all_possible.py](str_all_possible.py)
---
+++ chain.py
@@ -0,0 +1,55 @@
+#
+# This is an example of how to write a chain API.
+# This is an important skill when designing DSL (https://en.wikipedia.org/wiki/Domain-specific_language)
+#
+
+class RangeBuilder(object):
+
+    def __init__(self):
+        self.start = 0
+        self.end = 0
+        self.range_step = 1
+        self.include_last = False
+        self.reversed = False
+
+    def from_(self, start): # from cannot be used as a method name since it is a keyword in Python
+        self.start = start
+        return self
+
+    def to(self, end):
+        self.end = end
+        return self
+
+    def withLast(self):
+        self.include_last = True
+        return self
+
+    def step(self, step):
+        self.range_step = step
+        return self
+
+    def reverse(self):
+        self.reversed = not self.reversed
+        return self
+
+    def build(self):
+        if not self.reversed:
+            start = self.start
+            end = self.end
+            step = self.range_step
+            if self.include_last:
+                end += 1
+        else:
+            start = self.end
+            end = self.start
+            step = -self.range_step
+            if self.include_last:
+                end -= 1
+
+        return range(start, end, step)
+
+
+
+if __name__ == '__main__':
+    for i in RangeBuilder().from_(0).to(6).withLast().reverse().build():
+        print(i)
